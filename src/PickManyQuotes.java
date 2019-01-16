693a9149b4909d0fdc9b566d53aa0ff45d4230d7
Robert Chatley <rbc@imperial.ac.uk>
2019-01-16 11:42:57 +0000

    Initial files for exercise.

---
+++ src/PickManyQuotes.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PickManyQuotes {
+
+  public static void main(String[] args) throws Exception {
+
+    // Use file name and quote numbers in args
+    // to first create a list of quotes using loadQuotes
+    // and then print to System.out the chosen quotes.
+
+ }
+
+ /** Remove comment only after having written class Quote.
+
+  public static void reformat(List<Quote> quotes, int[] choices) {
+
+    // Print to System.out quotes according to choices.
+
+
+  }
+
+  public static List<Quote> loadQuotes(BufferedReader in) throws IOException {
+
+      List<Quote> quotes = new ArrayList<Quote>();
+
+    // Read lines from the input
+    // Create a Quote object for each line and store in list.
+
+    return quotes;
+  }
+
+  **/
+}
+
