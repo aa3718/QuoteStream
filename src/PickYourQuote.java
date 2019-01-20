5e8f5ca0f26bc6b314cf4672f59c0243990de37b
aa3718 <aa3718@ic.ac.uk>
2019-01-20 17:23:43 +0000

    reformat1 *_* <3

--- src/PickYourQuote.java
+++ src/PickYourQuote.java
@@ -4,34 +4,32 @@ import java.io.IOException;
 
 public class PickYourQuote {
 
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-        final String quotesFile = args[0];
-        int numberQuote = Integer.parseInt(args[1]);
+    final String quotesFile = args[0];
+    int numberQuote = Integer.parseInt(args[1]);
 
-        FileReader flrd = new FileReader(quotesFile);
-        BufferedReader buff = new BufferedReader(flrd);
+    FileReader flrd = new FileReader(quotesFile);
+    BufferedReader buff = new BufferedReader(flrd);
 
-        reformat(numberQuote, buff);
+    reformat(numberQuote, buff);
+  }
+
+  public static void reformat(int i, BufferedReader input) throws IOException {
+
+    String line = input.readLine();
+    int count = 1;
+
+    while (line != null) {
+      line = line.replace(";", "\n");
+
+      if (count == i) {
+        System.out.println(line);
+        return;
+      }
+
+      line = input.readLine();
+      count++;
     }
-
-    public static void reformat(int i, BufferedReader input) throws IOException {
-
-        String line = input.readLine();
-        int count = 1;
-
-        while (line != null) {
-            line = line.replace(";", "\n");
-
-            if (count == i) {
-                System.out.println(line);
-            return;
-            }
-
-            line = input.readLine();
-            count++;
-        }
-
-    }
-
+  }
 }
