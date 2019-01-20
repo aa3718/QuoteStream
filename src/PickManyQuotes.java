a55757f9d119c3a931c352615ca5a9f5f7c4280a
aa3718 <aa3718@ic.ac.uk>
2019-01-20 17:15:27 +0000

    reformat1

--- src/PickManyQuotes.java
+++ src/PickManyQuotes.java
@@ -9,43 +9,42 @@ import java.lang.Exception;
 
 public class PickManyQuotes {
 
-  public static void main(String[] args) throws Exception {
-     // assert args.length != 0 ;
-     // assert args.length != 1;
+    public static void main(String[] args) throws Exception {
+        // assert args.length != 0 ;
+        // assert args.length != 1;
 
-      if(args.length == 0 || args.length == 1){
-          return;
-      }
+        if (args.length == 0 || args.length == 1) {
+            return;
+        }
 
-      final String quotesFile = args[0];
+        final String quotesFile = args[0];
 
-      try {
-          FileReader flrd = new FileReader(quotesFile);
-          BufferedReader buff = new BufferedReader(flrd);
-          int [] anArray = new int[(args.length - 1)];
-          int j = 0;
+        try {
+            FileReader flrd = new FileReader(quotesFile);
+            BufferedReader buff = new BufferedReader(flrd);
+            int[] anArray = new int[(args.length - 1)];
+            int j = 0;
 
-          for (int i = 1; i < args.length ; i++) {
-              anArray[j] = Integer.parseInt(args[i]);
-              j++;
-          }
+            for (int i = 1; i < args.length; i++) {
+                anArray[j] = Integer.parseInt(args[i]);
+                j++;
+            }
 
-          reformat(loadQuotes(buff), anArray);
+            reformat(loadQuotes(buff), anArray);
 
-      } catch (FileNotFoundException fe){
+        } catch (FileNotFoundException fe) {
 
-          return;
-      }
-
-
-
- }
+            return;
+        }
 
 
-  public static void reformat(List<Quote> quotes, int[] choices) {
+    }
 
-      for (int i = 0; i < choices.length ; i++) {
-System.out.println(choices[i]);
+
+    public static void reformat(List<Quote> quotes, int[] choices) {
+
+        for (int i = 0; i < choices.length; i++) {
+            System.out.println(choices[i]);
 /*
 try {
     assert choices[i] <= quotes.size() && choices[i] > 1;
@@ -55,31 +54,31 @@ return;
 }
 */
 
-          if (choices[i] > quotes.size() || choices[i] < 1) {
-            return;
-          }
+            if (choices[i] > quotes.size() || choices[i] < 1) {
+                return;
+            }
 
-          String wruble = quotes.get(choices[i]-1).toString();
-          System.out.println(wruble);
-  }
+            String wruble = quotes.get(choices[i] - 1).toString();
+            System.out.println(wruble);
+        }
 
-  }
+    }
 
-  public static List<Quote> loadQuotes(BufferedReader input) throws IOException {
+    public static List<Quote> loadQuotes(BufferedReader input) throws IOException {
 
-      List<Quote> quotes = new ArrayList<Quote>();
+        List<Quote> quotes = new ArrayList<Quote>();
 
-      String line = input.readLine();
+        String line = input.readLine();
 
-      while (line != null) {
-          Quote sentence = new Quote(line);
-          quotes.add(sentence);
-          line = input.readLine();
-      }
+        while (line != null) {
+            Quote sentence = new Quote(line);
+            quotes.add(sentence);
+            line = input.readLine();
+        }
 
 
-    return quotes;
-  }
+        return quotes;
+    }
 
 }
 
--- src/Quote.java
+++ src/Quote.java
@@ -1,29 +1,29 @@
-    public class Quote {
+public class Quote {
 
     public String first;
     public String extra;
 
 
-        public Quote(String line) {
+    public Quote(String line) {
 
-            int middle = line.indexOf(";");
-            first = line.substring(0, middle);
-            middle++;
-            int length = line.length();
-            extra = line.substring(middle, length);
-        }
+        int middle = line.indexOf(";");
+        first = line.substring(0, middle);
+        middle++;
+        int length = line.length();
+        extra = line.substring(middle, length);
+    }
 
-        public String getText() {
-            return first;
-        }
+    public String getText() {
+        return first;
+    }
 
-        public String getComment() {
-            return extra;
-        }
+    public String getComment() {
+        return extra;
+    }
 
-            public String toString() {
-                String ruble = "'" + first + "' by " + extra;
-                return ruble;
-            }
+    public String toString() {
+        String ruble = "'" + first + "' by " + extra;
+        return ruble;
+    }
 
-        }
+}
