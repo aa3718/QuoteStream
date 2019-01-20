ca1a5daffeb77602b4dbaeba0090c4e5eb80934a
aa3718 <aa3718@ic.ac.uk>
2019-01-20 17:22:06 +0000

    reformat1 *_*

--- src/PickManyQuotes.java
+++ src/PickManyQuotes.java
@@ -6,79 +6,71 @@ import java.util.List;
 import java.io.FileNotFoundException;
 import java.lang.Exception;
 
-
 public class PickManyQuotes {
 
-    public static void main(String[] args) throws Exception {
-        // assert args.length != 0 ;
-        // assert args.length != 1;
+  public static void main(String[] args) throws Exception {
+    // assert args.length != 0 ;
+    // assert args.length != 1;
 
-        if (args.length == 0 || args.length == 1) {
-            return;
-        }
-
-        final String quotesFile = args[0];
-
-        try {
-            FileReader flrd = new FileReader(quotesFile);
-            BufferedReader buff = new BufferedReader(flrd);
-            int[] anArray = new int[(args.length - 1)];
-            int j = 0;
-
-            for (int i = 1; i < args.length; i++) {
-                anArray[j] = Integer.parseInt(args[i]);
-                j++;
-            }
-
-            reformat(loadQuotes(buff), anArray);
-
-        } catch (FileNotFoundException fe) {
-
-            return;
-        }
-
-
+    if (args.length == 0 || args.length == 1) {
+      return;
     }
 
+    final String quotesFile = args[0];
 
-    public static void reformat(List<Quote> quotes, int[] choices) {
+    try {
+      FileReader flrd = new FileReader(quotesFile);
+      BufferedReader buff = new BufferedReader(flrd);
+      int[] anArray = new int[(args.length - 1)];
+      int j = 0;
 
-        for (int i = 0; i < choices.length; i++) {
-            System.out.println(choices[i]);
-/*
-try {
-    assert choices[i] <= quotes.size() && choices[i] > 1;
-} catch (AssertionError e) {
+      for (int i = 1; i < args.length; i++) {
+        anArray[j] = Integer.parseInt(args[i]);
+        j++;
+      }
 
-return;
+      reformat(loadQuotes(buff), anArray);
+
+    } catch (FileNotFoundException fe) {
+
+      return;
+    }
+  }
+
+  public static void reformat(List<Quote> quotes, int[] choices) {
+
+    for (int i = 0; i < choices.length; i++) {
+      System.out.println(choices[i]);
+      /*
+      try {
+          assert choices[i] <= quotes.size() && choices[i] > 1;
+      } catch (AssertionError e) {
+
+      return;
+      }
+      */
+
+      if (choices[i] > quotes.size() || choices[i] < 1) {
+        return;
+      }
+
+      String wruble = quotes.get(choices[i] - 1).toString();
+      System.out.println(wruble);
+    }
+  }
+
+  public static List<Quote> loadQuotes(BufferedReader input) throws IOException {
+
+    List<Quote> quotes = new ArrayList<Quote>();
+
+    String line = input.readLine();
+
+    while (line != null) {
+      Quote sentence = new Quote(line);
+      quotes.add(sentence);
+      line = input.readLine();
+    }
+
+    return quotes;
+  }
 }
-*/
-
-            if (choices[i] > quotes.size() || choices[i] < 1) {
-                return;
-            }
-
-            String wruble = quotes.get(choices[i] - 1).toString();
-            System.out.println(wruble);
-        }
-
-    }
-
-    public static List<Quote> loadQuotes(BufferedReader input) throws IOException {
-
-        List<Quote> quotes = new ArrayList<Quote>();
-
-        String line = input.readLine();
-
-        while (line != null) {
-            Quote sentence = new Quote(line);
-            quotes.add(sentence);
-            line = input.readLine();
-        }
-
-
-        return quotes;
-    }
-
-}
-
--- src/QuotesPrinter.java
+++ src/QuotesPrinter.java
@@ -4,28 +4,23 @@ import java.io.IOException;
 
 public class QuotesPrinter {
 
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-        final String quotesFile = "Quotes.txt";
-        FileReader flrd = new FileReader(quotesFile);
-        BufferedReader buff = new BufferedReader(flrd);
+    final String quotesFile = "Quotes.txt";
+    FileReader flrd = new FileReader(quotesFile);
+    BufferedReader buff = new BufferedReader(flrd);
 
+    reformat(buff);
+  }
 
-        reformat(buff);
+  public static void reformat(BufferedReader input) throws IOException {
 
+    String line = input.readLine();
+
+    while (line != null) {
+      line = line.replace(";", "\n");
+      System.out.println(line);
+      line = input.readLine();
     }
-
-    public static void reformat(BufferedReader input) throws IOException {
-
-        String line = input.readLine();
-
-        while (line != null) {
-            line = line.replace(";", "\n");
-            System.out.println(line);
-            line = input.readLine();
-        }
-
-
-    }
+  }
 }
-
